65512f3f0caf586f729c7d5584fd928e3fef3b36
MoSedky <sedky.cs@gmail.com>
2018-12-19 17:43:58 +0300

    third commit Auto Approval -- Covering All Cases

--- Auto_Approval_results.py
+++ Auto_Approval_results.py
@@ -37,13 +37,20 @@ def auto_approval_form():
     # if all_partners_individuals == 1:
     #     print('No Attachment Needed; Send OTP to Individual Partners')
     # else:
+        if validate_after_auto_approval.partners_gov_ecr_saudi == 1:
+            #   & validate_after_auto_approval.partners_cmp_saudi_more_than_one == 0:
+            print('1.2. Attachment Needed, Send Request to Contract Reviewer then OTP Auto Approval')
 
-        if validate_after_auto_approval.partners_cmp_saudi_more_than_one == 0:
+        # elif (validate_after_auto_approval.partners_cmp_saudi_more_than_one == 0) \
+        #         & (validate_after_auto_approval.partners_gov_ecr_saudi == 0):
+        #
+        #     print('1.3. No Attachments Needed, Send Request to Contract Reviewer then OTP Auto Approval ')
+        # elif (validate_after_auto_approval.partners_cmp_saudi_more_than_one == 0) \
+        #         & (validate_after_auto_approval.partners_gov_ecr_saudi == 1):
+        #     print('1.4. Attachment Needed, Send Request to Contract Reviewer OTP Auto Approval ')
 
-            print(' No Attachments Needed, Send Request to Contract Reviewer')
-
-        else:
-            print('Go to Validate Managers of Company has more than one Partners')
+        if validate_after_auto_approval.partners_cmp_saudi_more_than_one == 1:
+            print('1.1.1. Go to Validate Managers of Company has more than one Partners')
             cmp_more_than_one_cmp_has_saudi_manager_has_privileges = input("Are one of partners is cmp partners "
                                                                            "with more than one partner "
                                                                            "and has saudi manager has privileges ? ")
@@ -56,13 +63,13 @@ def auto_approval_form():
                 ((cmp_more_than_one_cmp_has_saudi_manager_has_privileges == 0) &
                 (cmp_more_than_one_cmp_has_resident_manager_has_privileges == 0))):
 
-                print('Go to Validate Partners')
+                print('1.1.2. Go to Validate Partners')
                 validate_partners_partner_cmp()
 
             elif ((validate_after_auto_approval.partners_cmp_saudi_more_than_one == 1) &
                   ((cmp_more_than_one_cmp_has_saudi_manager_has_privileges == 1) |
                   (cmp_more_than_one_cmp_has_resident_manager_has_privileges == 1))):
-                print('No documents Needed, Send Request to Contract Reviewer then OTP To Saudi or Resident')
+                print('1.5. No documents Needed, Send Request to Contract Reviewer then OTP To Saudi or Resident')
 
 
 def validate_partners_partner_cmp():
@@ -84,28 +91,28 @@ def validate_partners_partner_cmp():
                                                         "and partner type Not Saudi, Nor ECR, One Saudi?")
 
     if cmp_partner_partner_type_has_different_type == 0:
-        print('No documents Needed. Send Request to Contract Reviewer then '
+        print('1.1.2. No documents Needed. Send Request to Contract Reviewer then '
               'Send OTP to ECR Owner, Saudi Individuals or One Company Partner Owner')
 
     else:
-        print('Please Attach Approval Documents and Send the Request to Contract Reviewer ')
+        print('1.1.3. Please Attach Approval Documents and Send the Request to Contract Reviewer ')
 
 
 Need_Auto_Approval = pre_condition()
 
 if Need_Auto_Approval == 1:
-    print('Yes Need Auto Approval, Lets validate Validation Cases for Reviewing')
+    print('1.1. Yes Need Auto Approval, Lets validate Validation Cases for Reviewing')
 
     Need_Reviewing = validate_after_auto_approval()
 
     if Need_Reviewing == 1:
-        print('Directly to Auto Approval')
+        print('1. Directly to Auto Approval')
     else:
-        print('Reviewing needed by Contract Reviewer then Auto Approval')
+        print('2. Reviewing needed by Contract Reviewer then Auto Approval')
     auto_approval_form()
 
 else:
-    print('No Auto Approval Needed, Request will assigned to Contract Reviewer then to Moj User')
+    print('3. No Auto Approval Needed, Request will assigned to Contract Reviewer then to Moj User')
 
 
 
